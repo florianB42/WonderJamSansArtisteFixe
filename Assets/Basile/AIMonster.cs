c07b2e4cbdd967a461caf666a34a4fd90c34586b
Basile <ngy.b@hotmail.com>
2021-02-26 22:39:58 -0500

    Animation walk Reaper + attack joueur à proximité

--- Assets/Basile/AIMonster.cs
+++ Assets/Basile/AIMonster.cs
@@ -7,10 +7,12 @@ public class AIMonster : MonoBehaviour
 {
     public Transform player;
     private NavMeshAgent agent;
+    private Animator animator;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
     }
@@ -19,5 +21,25 @@ public class AIMonster : MonoBehaviour
     {
         agent.destination = player.position;
        // transform.LookAt(playerTransform);
+    }
+
+    public void stopEnnemy()
+    {
+        agent.destination = transform.position;
+    }
+
+    public void EnnemyFollowing()
+    {
+        agent.destination = player.position;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision detected");
+        if (collision.gameObject.layer == 20)
+        {
+            animator.SetBool("PlayerInRange", true);
+            //TODO appel de la fonction GameOver et mort du joueur
+        }
     }
 }
